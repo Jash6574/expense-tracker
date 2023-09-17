a43583f64b51a21cd04d308e63922b9773a9b562
jash6574 <jashmavani6574@gmail.com>
2023-09-17 15:13:06 +0530

    ui

--- src/components/List.js
+++ src/components/List.js
@@ -36,9 +36,9 @@ function Transaction({ category }) {
   if (!category) return null;
   return (
     <>
-      <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{borderRight:`8px solid ${category.color ?? "#e5e5e5"}`}}>
-        <button className="px-1 mx-3"><box-icon name="trash" size={15} color={'#FF0000'}/></button>
+      <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{borderLeft:`8px solid ${category.color ?? "#e5e5e5"}`}}>
         <span className="block w-full">{category.name??""}</span>
+        <button className="px-1 mx-3"><box-icon name="trash" size={15} color={'#FF0000'}/></button>
       </div>
     </>
   );
