f41d9fe9c599672ab7ea2a11941090dd4b51e58a
jash6574 <jashmavani6574@gmail.com>
2023-09-18 12:19:01 +0530

    done

--- Expense-tracker/server/controller/controller.js
+++ Expense-tracker/server/controller/controller.js
@@ -1,110 +1,90 @@
-const model = require("../models/model");
+const model = require('../models/model');
 
-//post:Catagories
-async function create_Categories(req, res) {
-  try {
-    const Create = await model.Categories.create({
-      type: "Savings",
-      color: "#1F3B5C",
-    });
-    Create.save();
-    return res.json(Create);
-  } catch (err) {
-    return res.status(400).json({ message: `ERROR: ${err}` });
-  }
+//  post: http://localhost:8080/api/categories
+async function create_Categories(req, res){
+   const Create = new model.Categories({
+       type: "Investment",
+       color: "#FCBE44"
+   })
+
+   await Create.save(function(err){
+       if (!err) return res.json(Create);
+       return res.status(400).json({ message : `Error while creating categories ${err}`});
+   });
 }
 
-//get:Categories
-async function get_Categories(req, res) {
-  try {
-    const data = await model.Categories.find({});
+//  get: http://localhost:8080/api/categories
+async function  get_Categories(req, res){
+    let data = await model.Categories.find({})
 
-    const filter = await data.map((v) =>
-      Object.assign({}, { type: v.type, color: v.color })
-    );
+    let filter = await data.map(v => Object.assign({}, { type: v.type, color: v.color}));
     return res.json(filter);
-  } catch (err) {
-    return res.status(400).json({ message: `ERROR: ${err}` });
-  }
 }
 
-//post:transaction
-async function create_Transaction(req, res) {
-  try {
+//  post: http://localhost:8080/api/transaction
+async function create_Transaction(req, res){
+    if(!req.body) return res.status(400).json("Post HTTP Data not Provided");
     let { name, type, amount } = req.body;
-    const Create = await model.Transaction({
-      name,
-      type,
-      amount,
-      date: new Date(),
+
+    const create = await new model.Transaction(
+        {
+            name,
+            type,
+            amount,
+            date: new Date()
+        }
+    );
+
+    create.save(function(err){
+        if(!err) return res.json(create);
+        return res.status(400).json({ message : `Erro while creating transaction ${err}`});
     });
-    Create.save();
-    return res.json(Create);
-  } catch (err) {
-    return res.status(400).json({ message: `ERROR: ${err}` });
-  }
+
 }
 
-//get:transaction
-async function get_Transaction(req, res) {
-  try {
-    const data = await model.Transaction.find({});
+//  get: http://localhost:8080/api/transaction
+async function get_Transaction(req, res){
+    let data = await model.Transaction.find({});
     return res.json(data);
-  } catch (err) {
-    return res.status(400).json({ message: `ERROR: ${err}` });
-  }
 }
 
-//delete:transaction
-async function delete_Transaction(req, res) {
-  try {
-    await model.Transaction.deleteOne(req.body);
-    return res.json("Record Deleted...");
-  } catch (err) {
-    return res.status(400).json({ message: `ERROR: ${err}` });
-  }
+//  delete: http://localhost:8080/api/transaction
+async function delete_Transaction(req, res){
+    if (!req.body) res.status(400).json({ message: "Request body not Found"});
+    await model.Transaction.deleteOne(req.body, function(err){
+        if(!err) res.json("Record Deleted...!");
+    }).clone().catch(function(err){ res.json("Error while deleting Transaction Record")});
 }
 
-//get:labels
-async function get_Labels(req, res) {
-  try {
-    await model.Transaction.aggregate([
-      {
-        $lookup: {
-          from: "categories",
-          localField: "type",
-          foreignField: "type",
-          as: "categories_info",
+//  get: http://localhost:8080/api/labels
+async function get_Labels(req, res){
+
+    model.Transaction.aggregate([
+        {
+            $lookup : {
+                from: "categories",
+                localField: 'type',
+                foreignField: "type",
+                as: "categories_info"
+            }
         },
-      },
-      {
-        $unwind: "$categories_info",
-      },
-    ]).then((result) => {
-      let data = result.map((v) =>
-        Object.assign(
-          {},
-          {
-            _id: v._id,
-            name: v.name,
-            type: v.type,
-            amount: v.amount,
-            color: v.categories_info['color'],
-          }
-        )
-      );
-      res.json(data);
-    });
-  } catch (err) {
-    return res.status(400).json({ message: `LOOPUP ERROR: ${err}` });
-  }
+        {
+            $unwind: "$categories_info"
+        }
+    ]).then(result => {
+        let data = result.map(v => Object.assign({}, { _id: v._id, name: v.name, type: v.type, amount: v.amount, color: v.categories_info['color']}));
+        res.json(data);
+    }).catch(error => {
+        res.status(400).json("Looup Collection Error");
+    })
+
 }
 
 module.exports = {
-  create_Categories,
-  get_Categories,
-  create_Transaction,
-  get_Transaction,
-  delete_Transaction,
-  get_Labels,
-};
+    create_Categories,
+    get_Categories,
+    create_Transaction,
+    get_Transaction,
+    delete_Transaction,
+    get_Labels
+}
--- Expense-tracker/server/db/connection.js
+++ Expense-tracker/server/db/connection.js
@@ -1,12 +1,11 @@
-const { error } = require('console');
 const mongoose = require('mongoose');
-const conn = mongoose.connect(process.env.ATLAS_URI)
-.then(db => {
-    console.log("Database Connected Successfully!!!");
-    return db;
-}).catch(error => {
-    console.log("OOPSS Something Went Wrong to Connection");
-})
 
+const conn = mongoose.connect(process.env.ATLAS_URI)
+        .then(db => {
+            console.log("Database Connected");
+            return db;
+        }).catch(err => {
+            console.log("Connection Error");
+        })
 
 module.exports = conn;
